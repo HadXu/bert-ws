63a8bb8021fdea7fd58cddb2067cc861b044b82f
hadxu <hadxu123@gmail.com>
2020-03-17 21:28:26 +0800

    add example nn

---
+++ models.py
@@ -0,0 +1,20 @@
+from torch import nn
+import torch
+
+
+class ProteinNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input):
+        return input
+
+
+if __name__ == '__main__':
+    net = ProteinNet()
+    x = torch.rand((10, 5, 64))
+
+    y = net(x)
+
+    print(y.size())
+
