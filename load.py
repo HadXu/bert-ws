81e038e69f23cfb2d55559e1e5f3e24bb0072846
hadxu <hadxu123@gmail.com>
2020-03-26 16:07:48 +0800

    add CRF model

--- load.py
+++ load.py
@@ -2,6 +2,10 @@ import numpy as np
 import torch
 from torch.utils.data import DataLoader, Dataset
 
+START_TAG = "<START>"
+STOP_TAG = "<STOP>"
+tag_to_ix = {0: 0, 1: 1, 2: 2, START_TAG: 3, STOP_TAG: 4}
+
 
 class ProteinDataSet(Dataset):
     def __init__(self, filename):
@@ -13,10 +17,10 @@ class ProteinDataSet(Dataset):
         label = self.labels[x]
 
         feat = np.stack(feat)  # (L, 64)
-        label = np.array(label)  # (L, )
+        label = np.array(label, dtype=np.int64)  # (L, )
 
-        print(feat.shape)
-        print(label.shape)
+        # print(feat.shape)
+        # print(label.shape)
 
         return feat, label
 
@@ -58,5 +62,5 @@ if __name__ == '__main__':
     filename = "data/6125_validation_64.feat"
     loader = DataLoader(ProteinDataSet(filename), batch_size=1)
     for x, y in loader:
-        print(x)
-        print(y)
+        print(x.squeeze(0).size())
+        print(y.squeeze(0).size())
--- models.py
+++ models.py
@@ -15,30 +15,104 @@ def log_sum_exp(vec):
            torch.log(torch.sum(torch.exp(vec - max_score_broadcast)))
 
 
+START_TAG = "<START>"
+STOP_TAG = "<STOP>"
+
+
 class ProteinNet(nn.Module):
-    def __init__(self):
+    def __init__(self, hidden_dim=100):
         super(ProteinNet, self).__init__()
-        self.cnf = CNF()
+        self.tagset_size = 5
+        self.tag_to_ix = {START_TAG: 3, STOP_TAG: 4}
+        self.hidden_dim = hidden_dim
+
+        self.transitions = nn.Parameter(torch.randn(self.tagset_size, self.tagset_size))
+
+        self.transitions.data[self.tag_to_ix[START_TAG], :] = -10000
+        self.transitions.data[:, self.tag_to_ix[STOP_TAG]] = -10000
+
+        self.hidden = self.init_hidden()
+
+    def init_hidden(self):
+        return (torch.randn(2, 1, self.hidden_dim // 2),
+                torch.randn(2, 1, self.hidden_dim // 2))
 
     def forward(self, input):
-        out = self.cnf(input)
-        return out
+        score, tag_seq = self._viterbi_decode(input)
+        return score, tag_seq
 
+    def _viterbi_decode(self, feats):
+        backpointers = []
+        init_vvars = torch.full((1, self.tagset_size), -10000.)
+        init_vvars[0][self.tag_to_ix[START_TAG]] = 0
+        forward_var = init_vvars
+        for feat in feats:
+            bptrs_t = []
+            viterbivars_t = []
 
-class CNF(nn.Module):
-    def __init__(self):
-        super(CNF, self).__init__()
+            for next_tag in range(self.tagset_size):
+                next_tag_var = forward_var + self.transitions[next_tag]
+                best_tag_id = argmax(next_tag_var)
+                bptrs_t.append(best_tag_id)
+                viterbivars_t.append(next_tag_var[0][best_tag_id].view(1))
+            forward_var = (torch.cat(viterbivars_t) + feat).view(1, -1)
+            backpointers.append(bptrs_t)
 
-    def forward(self, input):
-        return input
+        terminal_var = forward_var + self.transitions[self.tag_to_ix[STOP_TAG]]
+        best_tag_id = argmax(terminal_var)
+        path_score = terminal_var[0][best_tag_id]
+
+        best_path = [best_tag_id]
+        for bptrs_t in reversed(backpointers):
+            best_tag_id = bptrs_t[best_tag_id]
+            best_path.append(best_tag_id)
+
+        start = best_path.pop()
+        assert start == self.tag_to_ix[START_TAG]  # Sanity check
+        best_path.reverse()
+        return path_score, best_path
+
+    def neg_log_likelihood(self, feats, tags):
+        forward_score = self._forward_alg(feats)
+        gold_score = self._score_sentence(feats, tags)
+        return forward_score - gold_score
+
+    def _forward_alg(self, feats):
+        init_alphas = torch.full((1, self.tagset_size), -10000.)
+        init_alphas[0][self.tag_to_ix[START_TAG]] = 0.
+
+        forward_var = init_alphas
+
+        for feat in feats:
+            alphas_t = []
+            for next_tag in range(self.tagset_size):
+                emit_score = feat[next_tag].view(
+                    1, -1).expand(1, self.tagset_size)
+                trans_score = self.transitions[next_tag].view(1, -1)
+                next_tag_var = forward_var + trans_score + emit_score
+                alphas_t.append(log_sum_exp(next_tag_var).view(1))
+            forward_var = torch.cat(alphas_t).view(1, -1)
+        terminal_var = forward_var + self.transitions[self.tag_to_ix[STOP_TAG]]
+        alpha = log_sum_exp(terminal_var)
+        return alpha
+
+    def _score_sentence(self, feats, tags):
+        score = torch.zeros(1)
+        tags = torch.cat([torch.tensor([self.tag_to_ix[START_TAG]], dtype=torch.long), tags])
+        for i, feat in enumerate(feats):
+            score = score + \
+                    self.transitions[tags[i + 1], tags[i]] + feat[tags[i + 1]]
+        score = score + self.transitions[self.tag_to_ix[STOP_TAG], tags[-1]]
+        return score
 
 
 def test_net():
     net = ProteinNet()
-    x = torch.rand((10, 5, 64))
-    y = net(x)
+    x = torch.rand((4, 64))
+    target = torch.tensor([0, 1, 2, 2], dtype=torch.long)
+    loss = net.neg_log_likelihood(x, target)
 
-    print(y.size())
+    print(loss.item())
 
 
 if __name__ == '__main__':
